03aa8e0694c8c108bba11d38353abfb4137c8af3
Litvak <kefirodua@gmail.com>
2022-10-02 17:16:34 +0300

    Initial commit

---
+++ src/ua/ithillel/java/Main.java
@@ -0,0 +1,12 @@
+package ua.ithillel.java;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		ValueCalculator o = new ValueCalculator();
+		o.runOneThread();
+		o.runTwoThreads();
+	}
+	
+}
